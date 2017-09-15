74a866c9c9acfd030984c99b32c8bf8fd1099852
NicoleIrene <nicoleirene89@gmail.com>
2017-09-15 14:07:48 -0400

    editing

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,6 @@
-
-  let bio = {
+/*Start of Resume 
+bio information*/
+let bio = {
     'name' : 'Nicole Irene',
     'role' : 'Front End Gladiator',
     'contacts' : {
@@ -13,19 +14,63 @@
     'welcomeMessage': 'Some days, you just have to create your own sunshine.',
     'skills': ['HTML5', 'CSS3', 'JavaScript', 'Wordpress'],
     'biopic': 'images/me.jpg'
-    'display': function () {
-
-    }
   };
   
+//bio funtion to display information
 
-
-
-  let formattedName = HTMLheaderName.replace('%data', bio.name);
+bio.display=function(){
+  let formattedName = HTMLheaderName.replace('%data%', bio.name);
   $('#header').prepend(formattedName);
 
   let formattedRole = HTMLheaderRole.replace('%data%', bio.role);
   $('#header').prepend(formattedRole);
 
   let formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-  $()
+  $('#topContacts').append(formattedMobile);
+  $('#footerContacts').append(formattedMobile);
+
+  let formattedEmail = HTMLemail.replace('%data%', bio.contacts.mobile);
+  $('#topContacts').append(formattedEmail);
+  $('#footerContacts').append(formattedEmail);
+
+  let formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+  $('#topContacts').append(formattedGithub);
+  $('#footerContacts').append(formattedGithub);
+
+  let formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+  $('#topContacts').append(formattedTwitter);
+  $('#footerContacts').append(formattedTwitter);
+
+  let formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+  $('#topContacts').append(formattedLocation);
+  $('#footerContacts').append(HTMLlocation);
+}
+bio.display(); //end of bio function
+
+//education information
+
+let education = {
+      'schools': [{
+        'name': 'Queens College at The City University of New York',
+        'location': 'Flushing, NY',
+        'degree': 'Bachelor of Arts',
+        'majors': ['Urban Studies'],
+        'dates': '2008-2013',
+        'url': string (optional)
+      }],
+
+    'onlineCourses': [{
+        'title': 'Front End Web Developer',
+        'school': 'Udacity',
+        'dates': 'May 2017 - Present',
+        'url': string
+    }],
+
+};
+
+//education function to display information
+education.display=function(){
+  let formattedName
+
+}
+education.display();
