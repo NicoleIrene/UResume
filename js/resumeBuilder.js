bb46d33cff142b0626774c44acfa092adb921158
NicoleIrene <nicoleirene89@gmail.com>
2017-09-18 18:44:05 -0400

    editing resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,72 +1,64 @@
 //bio information
 
 let bio = {
-  'name' : 'Nicole Irene',
-  'role' : 'Front End Gladiator',
-  'contacts' : {
-    'mobile': '0123456789',
-    'email': 'nicoleirene89@gmail.com', 
-    'github': 'github.com/NicoleIrene',
-    'twitter': '@NicoleIrene',
-    'location': 'New York City',
+  "name" : "Nicole Irene",
+  "role" : "Front End Gladiator",
+  "contacts" : {
+    "mobile": "0123456789",
+    "email": "nicoleirene89@gmail.com", 
+    "github": "github.com/NicoleIrene",
+    "twitter": "@NicoleIrene",
+    "location": "New York City",
   },
-  'welcomeMessage': 'Some days, you just have to create your own sunshine.',
-  'skills': ['HTML5', 'CSS3', 'JavaScript', 'Wordpress'],
-  'biopic': 'images/me.jpg'
-}
+  "welcomeMessage": "Some days, you just have to create your own sunshine.",
+  "skills": ["HTML5", "CSS3", "JavaScript", "Wordpress"],
+  "biopic": "images/me.jpg"
+};
 
-//bio funtion to display information
+//bio function to display information
 
 bio.display=function(){
-let formattedName = HTMLheaderName.replace('%data%', bio.name);
-$('#header').prepend(formattedName);
+  let formattedName = HTMLheaderName.replace('%data%', bio.name);
+  $('#header').prepend(formattedName);
 
-let formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-$('#header').prepend(formattedRole);
+  let formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+  $('#header').prepend(formattedRole);
 
-let formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-$('#topContacts').append(formattedMobile);
-$('#footerContacts').append(formattedMobile);
+  let formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+  $('#topContacts').append(formattedMobile);
+  $('#footerContacts').append(formattedMobile);
 
-let formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-$('#topContacts').append(formattedEmail);
-$('#footerContacts').append(formattedEmail);
+  let formattedEmail = HTMLemail.replace('%data%', bio.contacts.mobile);
+  $('#topContacts').append(formattedEmail);
+  $('#footerContacts').append(formattedEmail);
 
-let formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
-$('#topContacts').append(formattedGithub);
-$('#footerContacts').append(formattedGithub);
+  let formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+  $('#topContacts').append(formattedGithub);
+  $('#footerContacts').append(formattedGithub);
 
-let formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
-$('#topContacts').append(formattedTwitter);
-$('#footerContacts').append(formattedTwitter);
+  let formattedTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
+  $('#topContacts').append(formattedTwitter);
+  $('#footerContacts').append(formattedTwitter);
 
-let formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
-$('#topContacts').append(formattedLocation);
-$('#footerContacts').append(formattedLocation);
+  let formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+  $('#topContacts').append(formattedLocation);
+  $('#footerContacts').append(formattedLocation);
+};
 
-let formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-$('#header').prepend(formattedWelcomeMsg);
-
-let formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
-$('#header').prepend(formattedBioPic);
+bio.display(); //end of bio function
 
 if(bio.skills.length > 0) {
-$('header').append(HTMLskillsStart);
+$("header").append(HTMLskillsStart);
 
-let formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
-  $('#skills').append(formattedSkill);
-  formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
-  $('#skills').append(formattedSkill);
-  formattedSkill = HTMLskills.replace('%data%', bio.skills[2]);
-  $('#skills').append(formattedSkill);
-  formattedSkill = HTMLskills.replace('%data%', bio.skills[3]);
-  $('skills').append(formattedSkill);
-
-  }
-
-
+let formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+  $("#skills").append(formattedSkill);
+  formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+  $("skills").append(formattedSkill);
 };
-bio.display(); //end of bio function
 
 let education = {
   "schools": [
@@ -75,7 +67,7 @@ let education = {
     "location": "Flushing, NY",
     "degree": "Bachelor of Arts",
     "majors": ["Urban Studies"],
-    "dates": 2013,
+    "dates": "2013",
     "url": "http://www.qc.cuny.edu/Pages/home.aspx"
   },  
   {
@@ -83,11 +75,11 @@ let education = {
     "location": "Flushing, NY",
     "degree": "Bachelor of Arts",
     "majors": ["Urban Studies"],
-    "dates": 2013,
+    "dates": "2013",
     "url": "http://www.qc.cuny.edu/Pages/home.aspx"
   }
- ]
-},
+ ],
+
   "onlineCourses": [
     {
     "title": "Front End Web Developer",
@@ -96,21 +88,52 @@ let education = {
     "url": "string"
   }
  ]
-}
+};
 
+let work = {
+  "jobs": [
+  {
+    "employer": "Udacity",
+    "title": "Front End Web Developer",
+    "location": "NYC",
+    "dates": "2017",
+    "description": "fill this in later"
+  },
+  {
+    "employer": "Google",
+    "title": "Data Analyst",
+    "location": "NYC",
+    "dates": "2016",
+    "description": "fill this in later"
+  }
+ ]
+};
 
-// let work = {};
+for(job in work.jobs) {
+$("#workExperience").append(HTMLworkStart);
 
-// work.position = "Course Developer";
-// work.employer = "Udacity";
-// work.years = 0.3;
+let formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+let formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+let formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-// let education = {};
-// education['name'] = 'Queens College';
-// education['years'] = '2008-2013';
-// education['city'] = 'Flushing';
+$(".work-entry:last").append(formattedEmployerTitle);
 
-// $('#main').append(work['position']);
-// $('#main').append(education.name);
+ }
 
+let projects = {
+  "projects": [
+  {
+    "title": "Project 1",
+    "dates": "2017",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae ducimus eos voluptas illo voluptatum autem minus sint aspernatur adipisci sunt tempora, delectus officia optio suscipit. Neque dolor nobis enim, repudiandae?",
+    "images": ["images/crab.jpeg"]
+  },
+  {
+    "title": "Project 2",
+    "dates": "2017",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla voluptate quae praesentium quis dicta possimus sequi aliquid. Nulla sequi maxime voluptatem hic, quo sit voluptate, doloribus optio, sunt quaerat, assumenda.",
+    "images": ["images/flaminco.jpeg"]
 
+  }
+ ]
+};
